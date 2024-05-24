430b193b067db9e28e2f874e7088a8ba14d79017
Ivanna Navrotska <navrockaivanna15@gmail.com>
2024-05-24 13:34:30 +0300

    Update stack_heap_testing.c

--- stack_heap_testing.c
+++ stack_heap_testing.c
@@ -1,6 +1,8 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <psapi.h>
 
 // Custom type: Point
 typedef struct {
@@ -26,18 +28,36 @@ Point* createPointOnHeap(int x, int y) {
     return p;
 }
 
+// Function to get the current memory usage
+size_t getCurrentMemoryUsage() {
+    PROCESS_MEMORY_COUNTERS memInfo;
+    GetProcessMemoryInfo(GetCurrentProcess(), &memInfo, sizeof(memInfo));
+    return memInfo.WorkingSetSize;
+}
+
 void test_create_point_on_stack() {
+    size_t initial_memory = getCurrentMemoryUsage();
     Point p = createPointOnStack(1, 2);
+    size_t final_memory = getCurrentMemoryUsage();
+    // Assert no change in memory usage for stack allocation
+    assert(initial_memory == final_memory);
     assert(p.x == 1); // Assert x is correctly set
     assert(p.y == 2); // Assert y is correctly set
 }
 
 void test_create_point_on_heap() {
+    size_t initial_memory = getCurrentMemoryUsage();
     Point* p = createPointOnHeap(3, 4);
+    size_t after_alloc_memory = getCurrentMemoryUsage();
     assert(p != NULL); // Assert memory allocation succeeded
+    // Assert memory usage increased for heap allocation
+    assert(after_alloc_memory > initial_memory);
     assert(p->x == 3); // Assert x is correctly set
     assert(p->y == 4); // Assert y is correctly set
     free(p);           // Deallocate memory
+    size_t after_free_memory = getCurrentMemoryUsage();
+    // Assert memory usage decreased after free
+    assert(after_free_memory <= after_alloc_memory);
 }
 
 void run_test(void (*test_func)(), const char* test_name) {
